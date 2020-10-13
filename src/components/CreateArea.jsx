a984f13f866c6696e5fff7de6a9c76c3cfd3ca15
rajneesh <rajneesh@gmail.com>
2020-10-13 15:29:25 +0530

    Complete Keeper App

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import NoteAddIcon from "@material-ui/icons/NoteAdd";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+  const [isexpand, setexpand] = useState(false);
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+  function expand() {
+    setexpand(true);
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        {isexpand && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
+        <textarea
+          name="content"
+          onClick={expand}
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={isexpand ? 3 : 1}
+        />
+        <Zoom in={isexpand}>
+          <Fab onClick={submitNote}>
+            <NoteAddIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
